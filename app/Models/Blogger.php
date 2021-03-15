bf5aaaf6c72500516d73f0e39847d42df0e4593b
hassanfayyaz19 <hassanfayyaz19@gmail.com>
2021-03-15 22:52:04 +0500

    first commit

---
+++ app/Models/Blogger.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+class Blogger extends Authenticatable
+{
+    use HasFactory,Notifiable;
+
+    protected $guard = 'blogger';
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
